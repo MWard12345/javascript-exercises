eb7c505fe45f8236562ccfa6d6a05ee0818fdfa3
M Ward <mewlemons@yahoo.co.uk>
2023-08-17 13:40:59 +0100

    Will now pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = ""; 
+    if (num < 0) {
+        return "ERROR"; 
+    } else {
+        for (let i = 0; i < num; i++) {
+            repeatedString += string;
+        }
+        return repeatedString; 
+    }
 };
 
+//repeatString('hey', 3) // returns 'heyheyhey'
 // Do not edit below this line
 module.exports = repeatString;
