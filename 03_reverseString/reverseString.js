a217a7a39ff757ab9c76489d5a8542d7780805ea
M Ward <mewlemons@yahoo.co.uk>
2023-08-17 14:15:12 +0100

    Now passws

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function( word ) {
+    const charArray = word.split(""); 
+    const rArray = charArray.slice().reverse();
+    const rWord = rArray.join("");
+    return rWord;
 };
 
+
+// take string and reverse
+//how long is string
+//split into array
+//
 // Do not edit below this line
 module.exports = reverseString;
