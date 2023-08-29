cd772794b090b906f7a05f3f4ba65c65e1f24db9
M Ward <mewlemons@yahoo.co.uk>
2023-08-29 17:47:03 +0100

    Now passes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number" || isNaN(a) || isNaN(b)) {
+        return "ERROR";
+    }
+    if (a < 0 || b < 0 ) { return "ERROR" }
+    let sum = 0;
+    let startno = 0;
+    let endno = 0;
 
+    if (a < b) {
+        startno = a;
+        endno = b;
+    } else {
+        startno = b;
+        endno = a;
+    }
+
+    for (let i = startno; i <= endno; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 };
+
+
+
+
+// check if number, check - return ERROR
+
+// return asn
 
 // Do not edit below this line
 module.exports = sumAll;
