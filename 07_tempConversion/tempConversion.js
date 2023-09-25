2982471ca5ef098ff7b77974ef16bf241a28b8a2
M Ward <mewlemons@yahoo.co.uk>
2023-09-25 12:14:31 +0100

    Now passes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Temp) {
+  let converted=((((Temp-32)*5)/9)*10);
+  let answer=Math.round(converted);
+  return (answer/10);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Temp) {
+ let converted=(((Temp*1.8+32))*10);
+let  answer=Math.round(converted);
+ return (answer/10);
 };
 
+
+
+//f=temp*1.8+32
+//c=(temp-32)*5/9
+
+// let roundedNumber = 4.567.toFixed(2)
+// let roundedNumber = Math.round(4.5)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
