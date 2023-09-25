44d54565fae1d158153042c3c5eb38cc9c95cbaf
M Ward <mewlemons@yahoo.co.uk>
2023-09-25 12:45:26 +0100

    Now passes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+let sum = 1;
+  for (let i = 0; i < arr.length; i++) {
+    sum *= arr[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a**b);
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+let fac=1
+for (let i = 1; i <= a; i++) {
+  fac *= i;}
+return fac
+}
 
 // Do not edit below this line
 module.exports = {
