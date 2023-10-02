4acb7729372269fd44d53484cf6fc9e195657f3b
M Ward <mewlemons@yahoo.co.uk>
2023-10-02 09:37:52 +0100

    Now passes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const arrayWithSpaces = [...string];
+    const arrayWithoutSpaces = arrayWithSpaces.map(element => element.trim());
+    const arrayWithoutPunctuation = arrayWithoutSpaces.map(element => element.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ''));
+    const arrayWithoutNumbers = arrayWithoutPunctuation.filter(element => typeof element !== "number");
+    const cleanString= arrayWithoutNumbers.join("").toLowerCase();
+    const reversedString = cleanString.split('').reverse().join('');
+
+if (reversedString === cleanString) { return true;
+} else { return false ;
+}
 };
 
 // Do not edit below this line
