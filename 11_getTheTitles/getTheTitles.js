405489f5157bbca1dd22959ccd6b044593c300f8
M Ward <mewlemons@yahoo.co.uk>
2023-10-02 10:44:39 +0100

    Now passes

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+let titlesArray=[];
+for (let i = 0; i < books.length; i++) {
+    titlesArray.push(books[i].title);
+}
+return titlesArray;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
