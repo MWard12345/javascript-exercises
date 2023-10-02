a7011e25b3e4b06c3083c8e9914443e0e7ea487d
M Ward <mewlemons@yahoo.co.uk>
2023-10-02 10:21:34 +0100

    Now passes

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(F) {
+    if (!isNaN(F)) {
+        F = parseFloat(F); // Convert F to a number
+      } else {};
+    if (F < 1) { return "OOPS"} ;
+    if (F <= 1) { return (1) };
 
-};
+          let fib1 = 0;
+          let fib2 = 1;
+          let fibN = 0;
+      
+          for (let i = 2; i <= F; i++) {
+            fibN = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fibN;
+          }
+      
+          return fibN;
+        };
+      
+      
+ 
 
+
+
+// check string and convert
+// check neg and return "OOPS"
+// run equation
+// return result
+// {F_{n}=F_{n-1}+F_{n-2}}
 // Do not edit below this line
 module.exports = fibonacci;
